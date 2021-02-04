76380964db77f653afacc7f48d37617da8396dc1
Boba Fett <60700164+AndrewKepson@users.noreply.github.com>
2021-02-04 06:13:30 -0700

    Tailwind restyling

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,13 +7,22 @@ const AboutPage = () => {
   return (
     <Layout>
       <SEO title="About" />
-      <h1>About Me</h1>
-      <p>I grew up in Colorado, I love the Lord, play guitar, and run.</p>
-      <p>My guitars:</p>
-      <ul>
-        <li>Fender Stratocaster</li>
-        <li>Martin GPCPA4</li>
-      </ul>
+      <div class="container mx-auto px-4">
+        <div class="-mx-4 flex flex-wrap items-center">
+          <div class="mb-16 mt-16 px-4 w-full lg:w-8/12">
+            <p class="font-light leading-tight mb-6 text-3xl">
+              I love words, I love to build websites, and I love to tell
+              others&apos; stories.
+            </p>
+          </div>
+          <div class="px-4 w-full lg:w-4/12">
+            <img
+              src="https://images.unsplash.com/photo-1565972476522-4338522b1929?ixid=MXwyMDkyMnwwfDF8c2VhcmNofDV8fGJ1YmJsZXN8ZW58MHx8fA&ixlib=rb-1.2.1q=85&fm=jpg&crop=faces&cs=srgb&w=800&h=1200&fit=crop"
+              class="w-full"
+            />
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
