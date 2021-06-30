51ad4fe6c572c32a33af3acb547c9cd851c25f42
Boba Fett <60700164+AndrewKepson@users.noreply.github.com>
2021-06-29 20:35:41 -0600

    Updates click handling

--- src/components/contactform/submitButton.js
+++ src/components/contactform/submitButton.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const SubmitButton = ({ classes, handler }) => {
+const SubmitButton = ({ classes }) => {
   return (
-    <button className={classes} type="submit" onClick={handler}>
+    <button className={classes} type="submit">
       Submit
     </button>
   )
@@ -11,7 +11,6 @@ const SubmitButton = ({ classes, handler }) => {
 
 SubmitButton.propTypes = {
   classes: PropTypes.string.isRequired,
-  handler: PropTypes.func.isRequired,
 }
 
 export default SubmitButton
