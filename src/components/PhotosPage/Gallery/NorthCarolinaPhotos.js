e2ac88c93974581b2777efcc87fa90744124216d
Andrew Kepson <andyjkepson@gmail.com>
2021-11-28 07:30:28 -0700

    Removing extra components & addressing static image issue

--- src/components/PhotosPage/Gallery/NorthCarolinaPhotos.js
+++ src/components/PhotosPage/Gallery/NorthCarolinaPhotos.js
@@ -4,11 +4,13 @@ import { StaticImage } from 'gatsby-plugin-image'
 export default function NorthCarolinaPhotos() {
   return (
     <section className="grid grid-cols-1 md:grid-cols-3 md:grid-rows-3 gap-4 px-2">
-      <div className="md:col-span-4 md:row-span-2">
+      <div className="md:col-span-3 md:row-span-2">
         <StaticImage
           src="../../../images/portfolio-photos/north-carolina/blue-ridge-1.jpg"
-          className="rounded shadow-2xl"
+          className="rounded shadow-2xl object-cover"
           alt="The Blue Ridge Mountains in North Carolina"
+          placeholder="blurred"
+          layout="fullWidth"
           quality={90}
         />
       </div>
@@ -17,12 +19,16 @@ export default function NorthCarolinaPhotos() {
           src="../../../images/portfolio-photos/north-carolina/nc-fall-colors.jpg"
           className="rounded shadow-2xl"
           alt="The fall colors in North Carolina"
+          placeholder="blurred"
+          layout="constrained"
           quality={80}
         />
         <StaticImage
           src="../../../images/portfolio-photos/north-carolina/gatlinburg-river.jpg"
           className="rounded shadow-2xl"
-          alt="The fall colors in North Carolina"
+          alt="The fall colors in Tennessee at Great Smoky Mountains National Park"
+          placeholder="blurred"
+          layout="constrained"
           quality={80}
         />
       </div>
