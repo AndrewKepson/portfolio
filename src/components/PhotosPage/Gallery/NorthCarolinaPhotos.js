19b1606fc695553cee90e38f8e210c176001d549
Andrew Kepson <andyjkepson@gmail.com>
2021-11-28 07:41:04 -0700

    Replaced NC static images with gatsbyimage

--- src/components/PhotosPage/Gallery/NorthCarolinaPhotos.js
+++ src/components/PhotosPage/Gallery/NorthCarolinaPhotos.js
@@ -1,12 +1,45 @@
 import React from 'react'
-import { StaticImage } from 'gatsby-plugin-image'
+import { useStaticQuery, graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
 export default function NorthCarolinaPhotos() {
+  const data = useStaticQuery(graphql`
+    {
+      blueRidge: file(name: { eq: "blue-ridge-1" }) {
+        childImageSharp {
+          gatsbyImageData(
+            quality: 90
+            placeholder: BLURRED
+            formats: [AUTO, WEBP]
+          )
+        }
+      }
+      fallColors: file(name: { eq: "nc-fall-colors" }) {
+        childImageSharp {
+          gatsbyImageData(
+            quality: 60
+            placeholder: BLURRED
+            formats: [AUTO, WEBP]
+          )
+        }
+      }
+      gatlinburg: file(name: { eq: "gatlinburg-river" }) {
+        childImageSharp {
+          gatsbyImageData(
+            quality: 60
+            placeholder: BLURRED
+            formats: [AUTO, WEBP]
+          )
+        }
+      }
+    }
+  `)
+
   return (
     <section className="grid grid-cols-1 md:grid-cols-3 md:grid-rows-3 gap-4 px-2">
       <div className="md:col-span-3 md:row-span-2">
-        <StaticImage
-          src="../../../images/portfolio-photos/north-carolina/blue-ridge-1.jpg"
+        <GatsbyImage
+          image={getImage(data.blueRidge)}
           className="rounded shadow-2xl object-cover"
           alt="The Blue Ridge Mountains in North Carolina"
           placeholder="blurred"
@@ -15,16 +48,16 @@ export default function NorthCarolinaPhotos() {
         />
       </div>
       <div className="md:col-span-4 md:row-span-1 grid gap-4 md:grid-cols-2">
-        <StaticImage
-          src="../../../images/portfolio-photos/north-carolina/nc-fall-colors.jpg"
+        <GatsbyImage
+          image={getImage(data.fallColors)}
           className="rounded shadow-2xl"
           alt="The fall colors in North Carolina"
           placeholder="blurred"
           layout="constrained"
           quality={80}
         />
-        <StaticImage
-          src="../../../images/portfolio-photos/north-carolina/gatlinburg-river.jpg"
+        <GatsbyImage
+          image={getImage(data.gatlinburg)}
           className="rounded shadow-2xl"
           alt="The fall colors in Tennessee at Great Smoky Mountains National Park"
           placeholder="blurred"
