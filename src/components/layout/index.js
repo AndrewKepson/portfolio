6f90d46b0a8121dea57a12a53ead1d80667dd262
Andrew Kepson <andyjkepson@gmail.com>
2021-12-03 18:36:45 -0700

    Reimported components

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-import Header from '../Header/index'
-import Footer from '../Footer/index'
+import Header from '../Header'
+import Footer from '../Footer'
 
 export default function Layout({ children }) {
   return (
