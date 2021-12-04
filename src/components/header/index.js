3e5a65b2e7cb71b9e8f986884a3d9b20d9e40b75
Andrew Kepson <andyjkepson@gmail.com>
2021-12-03 18:23:54 -0700

    Adding components back

---
+++ src/components/header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import HeaderContent from './HeaderContent'
+
+const Header = () => (
+  <header className="relative sm:pt-6 pb-6 md:pb-12">
+    <HeaderContent />
+  </header>
+)
+
+export default Header
