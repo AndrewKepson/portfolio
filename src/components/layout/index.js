1fe38e623d3df0189abdcd4e1a9f310942130993
Andrew Kepson <andyjkepson@gmail.com>
2021-12-03 18:22:55 -0700

    Trying same with header and footer

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
 
 import Header from '../Header'
-import Main from './Main'
 import Footer from '../Footer'
 
 export default function Layout({ children }) {
   return (
     <div className="h-screen w-full p-0 m-0 overflow-x-hidden bg-gray-200">
       <Header />
-      <Main children={children} />
+      <div className="flex flex-col min-h-full text-gray-500 px-4 lg:px-16 lg:py-6">
+        <main className="h-100 flex justify-center pt-4 lg:pt-8">
+          {children}
+        </main>
+      </div>
       <Footer />
     </div>
   )
