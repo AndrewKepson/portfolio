82781fd57fca71e70d14989849454f87d049e97b
Andrew Kepson <andyjkepson@gmail.com>
2021-12-03 18:29:52 -0700

    Trying fix

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-import Header from '../Header'
-import Footer from '../Footer'
+import Header from '../Header/index'
+import Footer from '../Footer/index'
 
 export default function Layout({ children }) {
   return (
