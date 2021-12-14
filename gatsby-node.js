c71fa87ae6b755ec7d47c92c8f8b20aa84003fe8
AndrewKepson <60700164+AndrewKepson@users.noreply.github.com>
2021-12-13 20:49:35 -0700

    Removing extra stuff

--- gatsby-node.js
+++ gatsby-node.js
@@ -37,40 +37,10 @@ module.exports.createPages = async ({ graphql, actions }) => {
     }
   `)
 
-  const getSamples = await graphql(`
-    query {
-      allFile(filter: { relativeDirectory: { eq: "samples" } }) {
-        edges {
-          node {
-            childMarkdownRemark {
-              frontmatter {
-                date
-                title
-              }
-              fields {
-                slug
-              }
-            }
-          }
-        }
-      }
-    }
-  `)
-
   getBlogs.data.allFile.edges.forEach(edge => {
     createPage({
       component: path.resolve(`./src/templates/blog.js`),
       path: `/blog/${edge.node.childMarkdownRemark.fields.slug}`,
-      context: {
-        slug: edge.node.childMarkdownRemark.fields.slug,
-      },
-    })
-  })
-
-  getSamples.data.allFile.edges.forEach(edge => {
-    createPage({
-      component: path.resolve(`./src/templates/sample.js`),
-      path: `/writing/${edge.node.childMarkdownRemark.fields.slug}`,
       context: {
         slug: edge.node.childMarkdownRemark.fields.slug,
       },
