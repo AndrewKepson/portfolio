36c4965ab795ad46a500347c4057511b2bd180fa
AndrewKepson <60700164+AndrewKepson@users.noreply.github.com>
2021-12-20 11:47:05 -0700

    Updated image extensions to see if that's why netlify can't find them

--- src/components/PhotosPage/Gallery/ColoradoPhotos.js
+++ src/components/PhotosPage/Gallery/ColoradoPhotos.js
@@ -5,41 +5,38 @@ const ColoradoPhotos = () => (
   <section className="grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
     <div>
       <StaticImage
-        src="../../../images/portfolio-photos/colorado/MAROON_BELLS1.jpg"
+        src="../../../images/portfolio-photos/colorado/MAROON_BELLS1.JPG"
         quality={90}
         loading="lazy"
         placeholder="blurred"
-        className="rounded shadow"
+        className="rounded shadow-md"
         alt="The Maroon Bells in Aspen, Colorado"
       />
     </div>
     <div>
       <StaticImage
-        src="../../../images/portfolio-photos/colorado/VAIL1.jpg"
+        src="../../../images/portfolio-photos/colorado/VAIL1.JPG"
         quality={90}
         loading="lazy"
         placeholder="blurred"
-        className="rounded shadow"
+        className="rounded shadow-md"
         alt="Vail, Colorado"
       />
     </div>
     <div>
       <StaticImage
-        src="../../../images/portfolio-photos/colorado/VAIL2.jpg"
-        quality={90}
-        loading="lazy"
-        placeholder="blurred"
-        className="rounded shadow"
+        src="../../../images/portfolio-photos/colorado/VAIL2.JPG"
+        className="rounded shadow-md"
         alt="Vail, Colorado"
       />
     </div>
     <div>
       <StaticImage
-        src="../../../images/portfolio-photos/colorado/ASPENS.jpg"
+        src="../../../images/portfolio-photos/colorado/ASPENS.JPG"
         quality={90}
         loading="lazy"
         placeholder="blurred"
-        className="rounded shadow"
+        className="rounded shadow-md"
         alt="Aspen trees in Colorado"
       />
     </div>
