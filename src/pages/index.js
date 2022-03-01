917b234af53ce26bd160d948bd7f7abf723257d8
AndrewKepson <60700164+AndrewKepson@users.noreply.github.com>
2022-03-01 09:21:32 -0700

    Updating schema markup method - test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 import Seo from '../components/head'
 import HomePageContent from '../components/HomePage'
-import { PersonSchema } from '../components/Schema/Person'
-import {WebPageSchema} from '../components/Schema/WebPage'
 
-const IndexPage = () => {
-  console.log(PersonSchema)
+const IndexPage = ({ data }) => {
+  const {schemaMarkup} = data.wpPage.schemaMarkup
+
   return (
     <Layout>
       <Seo
@@ -15,10 +15,20 @@ const IndexPage = () => {
         description="Andrew Kepson is a digital marketing professional based in Colorado. In addition to SEO, he also enjoys trail running, reading, and occasional blogging."
       />
       <HomePageContent />
-      <PersonSchema />
-      <WebPageSchema name="Home - Andrew Kepson" description="Andrew Kepson is a digital marketing professional based in Colorado. In addition to SEO, he also enjoys trail running, reading, and occasional blogging." />
+      <div dangerouslySetInnerHTML={{ __html: schemaMarkup }} />
     </Layout>
   )
 }
 
 export default IndexPage
+
+export const query = graphql`
+query HomePageQuery {
+  wpPage(title: {eq: "Home"}) {
+    id
+    schemaMarkup {
+      schemaMarkup
+    }
+  }
+}
+`
