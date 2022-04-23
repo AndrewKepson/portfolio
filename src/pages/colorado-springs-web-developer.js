7009845737f1b273ba671aecaccdef269424b998
AndrewKepson <60700164+AndrewKepson@users.noreply.github.com>
2022-04-23 08:24:40 -0600

    Page query for new page in wp

--- src/pages/colorado-springs-web-developer.js
+++ src/pages/colorado-springs-web-developer.js
@@ -6,11 +6,74 @@ import {
   ColoradoSpringsPageContent,
 } from '../components/components'
 
-const ColoradoSpringsPage = () => (
+const ColoradoSpringsPage = ({
+  data: {
+    wpPage: {
+      featuredImage: {
+        altText,
+        node: {
+          localFile: {
+            childImageSharp: { ogImg, twitterImg },
+          },
+        },
+      },
+      seo,
+      schemaMarkup: { schemaMarkup },
+    },
+  },
+}) => (
   <Layout>
-    <Seo title="Colorado Springs Web Developer - Web Design in Colorado Springs" />
+    <Seo
+      title={seo.title}
+      description={seo.metaDesc}
+      canonical={seo.canonical}
+      ogImg={ogImg.src}
+      ogImgAltText={altText}
+      twitterImg={twitterImg.src}
+    />
     <ColoradoSpringsPageContent />
+    <div dangerouslySetInnerHTML={{ __html: schemaMarkup }} />
   </Layout>
 )
 
 export default ColoradoSpringsPage
+
+export const query = graphql`
+  query ColoradoSpringsWPPageQuery {
+    wpPage(title: { eq: "Colorado Springs Web Developer" }) {
+      id
+      featuredImage {
+        node {
+          localFile {
+            childImageSharp {
+              ogImg: resize(
+                cropFocus: ATTENTION
+                height: 630
+                width: 1200
+                quality: 90
+              ) {
+                src
+              }
+              twitterImg: resize(
+                cropFocus: ATTENTION
+                height: 120
+                width: 120
+                quality: 90
+              ) {
+                src
+              }
+            }
+          }
+        }
+      }
+      seo {
+        title
+        metaDesc
+        canonical
+      }
+      schemaMarkup {
+        schemaMarkup
+      }
+    }
+  }
+`
